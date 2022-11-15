21ad63aba1b90ed56abd481f7156f23a0cd02b99
Juan Manuel Monti <juanmanuel.monti@etermax.com>
2022-11-15 15:52:08 -0300

    Added navigation test between two fragments

--- app/src/main/java/com/etermax/preguntados/kata_news/presentation/lobby/LobbyFragment.kt
+++ app/src/main/java/com/etermax/preguntados/kata_news/presentation/lobby/LobbyFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.etermax.preguntados.kata_news.R
 import com.etermax.preguntados.kata_news.databinding.LobbyFragmentBinding
 
 class LobbyFragment : Fragment() {
@@ -17,5 +19,16 @@ class LobbyFragment : Fragment() {
     ): View {
         binding = LobbyFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.newsButton.setOnClickListener {
+            findNavController().navigate(R.id.newsDetailFragment)
+            //TODO: Averiguar como hacer esto mejor
+            //findNavController().navigate(RaceFragmentDirections.actionSceneFragmentToInfoFragmentV2())
+
+        }
     }
 }
